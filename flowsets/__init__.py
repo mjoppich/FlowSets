1558e04fb6566d79ad053f502cfa6efec3967a6b
mjoppich <git@mjoppich.net>
2025-12-05 13:58:46 +0100

    pypi project

---
+++ flowsets/__init__.py
@@ -0,0 +1,6 @@
+from flowsets.flowsets import SankeyPlotter
+from flowsets.flowsets import CustomFuzzyVar
+from flowsets.flowsets import BaseFuzzifier
+from flowsets.flowsets import LegacyFuzzifier
+from flowsets.flowsets import FastFuzzifier
+from flowsets.flowsets import FlowAnalysis
